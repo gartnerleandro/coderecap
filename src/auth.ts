e22a3a3aba3977def3faf22459490fd3ce191fe1
Leandro Gartner Hernández <gartnerleandro@gmail.com>
2024-12-09 11:54:04 +0100

    feat: Update github scope

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub({
-      authorization: { params: { scope: "repo" } },
+      authorization: { params: { scope: "repo:read" } },
     }),
   ],
   session: {
