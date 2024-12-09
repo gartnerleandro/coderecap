0652f7fcb29ba2822c8912e504642462cdc28a91
Leandro Gartner Hernández <gartnerleandro@gmail.com>
2024-12-09 11:59:58 +0100

    revert: github scope access

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub({
-      authorization: { params: { scope: "repo:read" } },
+      authorization: { params: { scope: "repo" } },
     }),
   ],
   session: {
